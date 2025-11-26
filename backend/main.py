5ed0ab285d41cb8171562978461a8c3be58bb1d7
Jahid H Emon <je2264r@gre.ac.uk>
2025-11-26 22:06:55 +0000

    fixed error

--- backend/main.py
+++ backend/main.py
@@ -12,10 +12,10 @@ app = FastAPI()
 # CORS for Netlify Frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Change "*" to your Netlify domain for higher security
+    allow_origins=["https://jemonportfolio.netlify.app"],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Content-Type", "Authorization"],
 )
 
 @app.get("/")
